f101b113fe608b4859f5ab700338ee2bda1f8ccb
Jonathan Shapiro <50190184+Jshap06@users.noreply.github.com>
2024-10-03 00:46:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,32 @@ app.use(express.json()); // Parse incoming JSON requests
 app.options('*', cors());
 app.use(cors());
 
+const encryptionKey = process.env.encryptionkey;
+
+function decryptDetails(details){
+    const bytes = CryptoJS.AES.decrypt(details.password, encryptionKey);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+    return(originalText)
+}
+
 
 app.post("/fulfillAxios",async(req,res)=>{
+  try{
   const details=req.body;
+  if(details.encrypted){
+    const password=decrypt(details);
+    details.xml=details.xml.replace(details.password,password)
+  }
   const response=await axios.post(details.url,details.xml,{headers: {
             'Content-Type': 'text/xml',
             "Cookie":"edupointkeyversion=pLbL29JuBFfT2HwPdgcQQmZQVePkoGBBVsLaB0ztBQC/jGmbFGAFzaaqIjVo1lxv;"
           }})
-  res.json(response.data);
+  res.json({status:true,response:response.data});
+  }catch(error){res.json({status:false,message:error.message})}})
+
+app.post("/encryptPassword",(req,res)=>{
+  const details=req.body;
+  res.json(encrpytedPassword:CryptoJS.AES.encrypt(details.password, encryptionKey).toString())
 })
 
 app.listen(3000, () => {
