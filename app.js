0e2f14872c7c6f5d3ee8ab9784c0a23e92b6d3ff
Jonat <jonathanshapiro789@gmail.com>
2024-11-03 00:54:51 -0400

    giga

--- app.js
+++ app.js
@@ -224,8 +224,9 @@ function parseClassData(data){
   data=data.reportCardScoreTypes;
   const gradeScale={};
   data[2].details.forEach(grade=>{
-    console.log(typeof(grade.lowScore));
+    console.log(grade)
       if(grade.lowScore>=0&&grade.highScore>=0){
+        console.log("bullshit")
         gradeScale[grade.score]=[grade.lowScore,grade.highScore];
 
   }});
